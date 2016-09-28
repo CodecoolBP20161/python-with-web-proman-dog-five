c4759731e4f7934500a395eca6c557c7441455b1
Komlancz Janos <jkomlancz@gmail.com>
2016-09-28 12:35:12 +0200

    click To hide works, update and list local storage are DONE

--- static/js/script.js
+++ static/js/script.js
@@ -3,42 +3,84 @@ var boards = [];
 function Board(title) {
     this.title = title;
 }
+//Save to local storage
+function saveBoardsToLocalStorage() {
+    var str = JSON.stringify(boards);
+    localStorage.setItem("boards", str);
+    showLastBoard();
+}
 
+//Remove board from LocalStorage
+function removeDataWithId(boardId) {
+    boards.slice(boardId, 1);
+    saveBoardsToLocalStorage();
+}
+
+//remove all Boards
+function removeAllBoards() {
+    localStorage.removeItem("boards");
+}
+
+
+//Get data from local storage
+function getDataFromLocalStorage() {
+    var storage_date = localStorage.getItem("boards");
+    boards = JSON.parse(storage_date);
+    if(!boards){
+        boards = [];
+    }
+    return boards;
+}
+
+//After add new Board show the last board
+function showLastBoard() {
+    var fromList = getDataFromLocalStorage();
+    var item = fromList[fromList.length-1];
+    var title = item.title;
+    $("<div id='board_"+ title + "' class='board'><p>"+title+"</p></div>").insertBefore("#add-board-title");
+
+}
+//Show all boards
+function listBoards() {
+    var fromList = getDataFromLocalStorage();
+    for (var i in fromList) {
+        var item = fromList[i];
+        var title = item.title;
+
+        var new_board = $("<div id='board_" + title + "' class='board'><p>" + title + "</p></div>");
+        new_board.insertBefore("#add-board-title");
+        clickToHide();
+    }
+}
+
+//clickToHideAllBoards
+function clickToHide() {
+    $('.board').click(function (event) {
+        event.preventDefault();
+        $('.board').hide(1000);
+        // removeAllBoards();
+    });
+}
 
 // Create new board object
 function addNewBoardWithTitle(title) {
     var board = new Board(title);
     boards.push(board);
-}
-
-// Get board by index
-function getBoardByIndex(index) {
-    return boards[index];
-}
-
-// List boards
-function appendBoards() {
-    for (var i in boards) {
-        // return boards[i].title
-        
-        // Depending to the end of what is already there
-        var board = boards[i];
-        var title = board.title;
-        // $("<p>"+title+"</p>").insertBefore("#board-input");
-    }
+    saveBoardsToLocalStorage();
 }
 
 
 $(document).ready(function(){
+    getDataFromLocalStorage();
+    listBoards();
+    alert(boards.length);
+
+
     $("#add-board-title").submit(function(event) {
         event.preventDefault(); //Prevents the default behavior, wouldn't refresh everything
-        
         var title = $("#board-input").val();
         addNewBoardWithTitle(title);
-        $("<div id='not-unique-id'><p>"+title+"</p></div>").insertBefore("#add-board-title");
+        clickToHide();
     })
-
-   
 });
-
 // THE DIV ID HAS TO BE CHANGED TO UNIQUE FOR EVERY NEW DIV !!!!!!!!!!!!!!
